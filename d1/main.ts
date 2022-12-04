6139208054e1d274825c9f2f7e4890abbbf0bfd2
Jan Kuzelik <honza.ku@gmail.com>
2022-12-04 11:23:09 +0100

    Improved day 1 code

--- d1/main.ts
+++ d1/main.ts
@@ -1,41 +1,12 @@
 // https://adventofcode.com/2022/day/1
-import { readLines } from 'https://deno.land/std@0.167.0/io/buffer.ts'
 
-const file = await Deno.open('input.txt')
+const file = await Deno.readTextFile('input.txt')
 
-const maxElves: Array<Array<number>> = [[], [], []]
-let currentElf: Array<number> = []
+const allElves = file
+  .split('\n\n')
+  .map((elf) => elf.split('\n'))
 
-for await (const line of readLines(file)) {
-  if (line.length == 0) {
-    const currentElfValue = currentElf.reduce((accumulator, value) => {
-      return +accumulator + +value
-    }, 0)
+const elfSums = allElves.map((elf) => elf.reduce((a, b) => a + +b, 0))
 
-    for (const elfIndex in maxElves) {
-      const topElfValue = maxElves[elfIndex].reduce((accumulator, value) => {
-        return +accumulator + +value
-      }, 0)
-      
-      if (currentElfValue > topElfValue) {
-        maxElves.splice(+elfIndex, 0, currentElf)
-        maxElves.splice(3, 1)
-        break
-      }
-    }
-    currentElf = []
-  } else {
-    currentElf.push(+line)
-  }
-}
-
-console.log(maxElves)
-
-console.log(
-  maxElves.flatMap(a => a)
-    .reduce((accumulator, value) => {
-      return +accumulator + +value
-    }, 0)
-)
-
-file.close()
+const topElves = elfSums.sort((a, b) => b-a).slice(0, 3)
+console.log(topElves.reduce((a, b) => a + b, 0))
